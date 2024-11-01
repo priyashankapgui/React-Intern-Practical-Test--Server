1b183a261e42ad8a836a912464c8063e82ece273
PRIYASHANKA.P.G.U.I <110629606+priyashankapgui@users.noreply.github.com>
2024-11-01 14:37:40 +0530

    new Mongo conection

--- config/database.js
+++ config/database.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
-MONGODB_URL="mongodb+srv://Ushangame:Q8nsCUzzr8XwrnOK@products.gay1l.mongodb.net/?retryWrites=true&w=majority&appName=Products"
 
-//Connect the DB
+// Use the actual MONGODB_URL from your environment variable
+const MONGODB_URL = "mongodb+srv://Ushangame:Q8nsCUzzr8XwrnOK@products.gay1l.mongodb.net/?retryWrites=true&w=majority&appName=Products";
+
+// Connect to the DB
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    // Connect without deprecated options
+    await mongoose.connect(MONGODB_URL);
     console.log("MongoDB connection success!");
   } catch (error) {
     console.error("MongoDB connection failed:", error);
